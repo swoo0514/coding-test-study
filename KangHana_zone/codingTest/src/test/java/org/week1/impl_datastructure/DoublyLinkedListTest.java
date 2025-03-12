9dbeac0f924ba336e6fd452f913c347bd094d825
kanghana1 <qazplm12093@naver.com>
2025-03-12 18:50:30 +0900

    complete ddl code

---
+++ KangHana_zone/codingTest/src/test/java/org/week1/impl_datastructure/DoublyLinkedListTest.java
@@ -0,0 +1,65 @@
+package org.week1.impl_datastructure;
+//package KangHana_zone.codingTest.src.test.java.org.week1.impl_datastructure;
+
+import KangHana_zone.codingTest.src.main.java.org.week1.impl_datastructure.DoublyLinkedList;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DoublyLinkedListTest {
+
+    private DoublyLinkedList<Integer> ddl;
+    @BeforeEach
+    void before() {
+        ddl = new DoublyLinkedList<>();
+    }
+
+    @Test
+    void size() {
+
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void search() {
+    }
+
+    @Test
+    void addFirst() {
+    }
+
+    @Test
+    void addLast() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void removeFirst() {
+    }
+
+    @Test
+    void removeLast() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void indexOf() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void clear() {
+    }
+}

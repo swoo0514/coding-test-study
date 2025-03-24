ebe5a3479bc3586e9d1e409de27d5bef70010a59
Lims <swoo0514lim@gmail.com>
2025-03-24 23:20:20 +0900

    [BOJ]: 2번 빠른 포기 후 자료구조로 런을 위한 커밋

---
+++ Limseungwoo_zone/week2/BOJ/2.cpp
@@ -0,0 +1,22 @@
+// BOJ 16236
+#include <bits/stdc++.h>
+#define my = 2
+using namespace std;
+int N;
+int maps[24][24];
+int dy[] = {1, 0, -1, 0};
+int dx[] = {0, 1, 0, -1};
+
+void bfs() {
+  int visited[24][24] = {
+      0,
+  };
+}
+
+int main() {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cin >> maps[i][j];
+    }
+  }
+}

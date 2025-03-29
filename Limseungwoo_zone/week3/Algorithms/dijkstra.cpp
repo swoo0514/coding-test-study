c19e99b6372a345c46a93a6a74ac7d876d28fdb9
Lims <swoo0514lim@gmail.com>
2025-03-30 00:30:32 +0900

    [CHORE]: commit everything

--- Limseungwoo_zone/week3/Algorithms/dijkstra.cpp
+++ Limseungwoo_zone/week3/Algorithms/dijkstra.cpp
@@ -4,14 +4,14 @@ using namespace std;
 const int INF = 1e9;
 const int MAX = 1001;
 
-vector<pair<int, int>> graph[MAX];
+vector<pair<int, int>> graph[MAX];  // 그래프 선언
 int dist[MAX];
 int n, m, start;
 void dijkstra(int start) {
   priority_queue<pair<int, int>, vector<pair<int, int>>,
                  greater<pair<int, int>>>
-      pq;
-  dist[start] = 0;
+      pq;           // 우선순위 큐를 사용하여 구현
+  dist[start] = 0;  // 시작지점은 가중치 0으로 세팅
   pq.push({0, start});
 
   while (!pq.empty()) {
@@ -24,7 +24,7 @@ void dijkstra(int start) {
     for (auto next : graph[cur]) {
       int next_node = next.first;
       int next_cost = next.second;
-
+      // 현재 가중치보다 다음 노드를 거치는 경우의 가중치가 더 작은 경우
       if (dist[next_node] > cost + next_cost) {
         dist[next_node] = cost + next_cost;
         pq.push({dist[next_node], next_node});

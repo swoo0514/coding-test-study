85437783a5c0a4a3959a261e2bdada8f60705fd2
Lims <swoo0514lim@gmail.com>
2025-05-27 01:25:12 +0900

    [BOJ]: gg 5.cpp

---
+++ Limseungwoo_zone/week8/5.cpp
@@ -0,0 +1,14 @@
+// BOJ 17135
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, d;
+int maps[20][20];
+int main() {
+  cin >> n >> m >> d;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> maps[i][j];
+    }
+  }
+}
